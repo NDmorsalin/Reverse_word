c85dca9ecada7572812f73bb1d4c6ae9c04136d5
NDmorsalin <morsalin056@gmail.com>
2020-02-08 08:57:39 +0600

    Reverse string

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
 
-console.log("Hello World");
+
+var str=" i love java script";
+function reverce(str){
+    var st="";//store yhe looped result 
+    for(var i=str.length-1;i>=0;i--){
+        // length of string is alaws 1 less 
+        st+=str[i];
+    }
+    return st;
+}
+var result = reverce(str);
+console.log(result);
+
+
+
+function reverceString(sr){
+    if(sr===""){
+        return "";
+    }else{
+        return reverceString(sr.substr(1))+sr.charAt(0);
+    }
+}
+console.log(reverceString(str));
+
+
+function methodReverce(str){
+    var arrString =str.split("");
+    var arrReverce = arrString.reverse();
+    var resultReverce = arrReverce.join("");
+    return resultReverce;
+    }
+console.log(methodReverce(str));
+
